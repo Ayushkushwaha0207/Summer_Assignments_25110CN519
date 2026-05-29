351a8c84c5d224c3bda7ede413247ba68a265817
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-05-29 23:43:39 +0530

    My 2nd commit of day 2 summer assignment

---
+++ DAY2/q6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num, temp, rev=0 ;
+    printf("Enter the number to reverse:");
+    scanf("%d", &num);
+    while(num!=0){
+        temp=num%10;
+        rev=rev*10+temp;
+        num=num/10;
+    }
+    printf("The reverse of number is %d.", rev);
+    return 0;
+}
