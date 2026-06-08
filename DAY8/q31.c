62b82096ce61644cc1db6ad59ccb0f2202e1ec5c
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-06-08 21:02:12 +0530

    My 3rd commit of DAY8 of summer assignment

---
+++ DAY8/q31.c
@@ -0,0 +1,14 @@
+//Write a program to Print character triangle. 
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter number of rows to be printed:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(char ch='A';ch<'A'+i;ch++){
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    return 0;
+}
