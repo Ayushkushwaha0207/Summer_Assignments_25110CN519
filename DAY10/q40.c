08de3eaef8817c42038bac95ec2d5459e6eac3f5
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-06-10 21:54:26 +0530

    My 4th commit of DAY10 of summer assignment

---
+++ DAY10/q40.c
@@ -0,0 +1,20 @@
+//Write a program to Print character pyramid.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of rows to be printed for character pyramid: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){ 
+        for (int j = 1; j <= n - i; j++){
+            printf(" ");
+        }        
+        for (int j = 1; j <= i; j++){
+            printf("%c", 'A' + j - 1);
+        }        
+        for (int j = i - 1; j >= 1; j--){
+            printf("%c", 'A' + j - 1);
+        }         
+        printf("\n");
+    }
+    return 0;
+}
