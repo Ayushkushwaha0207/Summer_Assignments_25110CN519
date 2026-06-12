2b63f9093154c8e59aeca3b2a843ea8fd6f304ab
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-06-12 20:47:48 +0530

    My 4th commit of DAY12 of summer assignment

---
+++ DAY12/q48.c
@@ -0,0 +1,25 @@
+//Write a program to Write function for perfect number.
+#include <stdio.h>
+int perfect(int num){
+    int sum=0;
+    for (int i=1; i<num; i++){
+        if (num%i==0){
+            sum+=i;
+        }
+    }
+    if(sum==num){
+        return 1;  
+    }else{
+        return 0; 
+    }
+}
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if (perfect(num))
+        printf("%d is a Perfect Number.",num);
+    else
+        printf("%d is not a Perfect Number.",num);
+    return 0;
+}
