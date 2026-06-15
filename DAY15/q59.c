4dbd048b696e230da71d289ea0f06dbe9f6ba8c6
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-06-15 21:43:28 +0530

    My 3rd commit of DAY15 of summer assignment

---
+++ DAY15/q59.c
@@ -0,0 +1,22 @@
+//Write a program to Rotate array right.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int temp = arr[n - 1];
+    for(int i = n - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+    printf("Array after right rotation:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
