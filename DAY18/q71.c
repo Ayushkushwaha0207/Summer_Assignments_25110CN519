c6dee2f85c64d84ce745a0413caf0e75e71e5c89
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-06-18 22:09:58 +0530

    My 3rd commit of DAY18 of summer assignment

---
+++ DAY18/q71.c
@@ -0,0 +1,24 @@
+//Write a program to Binary search. 
+#include<stdio.h>
+int main(){
+    int arr[]={10,20,30,40,50,60,70};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int key,low=0,high=n-1,mid;
+    printf("Enter the element neede to search:");
+    scanf("%d",&key);
+    while(low<=high){
+        mid=(low+high)/2;
+        if(arr[mid]==key){
+            printf("Element found at index %d",mid);
+            return 0;
+        }
+        else if(arr[mid]<key){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    printf("Element not found");
+    return 0;
+}
