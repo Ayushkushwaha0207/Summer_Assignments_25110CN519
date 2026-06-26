930ab8eb4a4000d5deb7e06b93ac0093c6c1c404
Ayushkushwaha0207 <ayushkush351@gmail.com>
2026-06-26 15:30:50 +0530

    My 4th commit of DAY26 of summer assignment

---
+++ DAY26/q104.c
@@ -0,0 +1,26 @@
+//Write a program to Create quiz application.
+#include<stdio.h>
+int main(){
+    int answer,score=0;
+    printf("Quiz Application\n");
+    printf("1.What is the capital of India?\n");
+    printf("1.Mumbai\n2.Delhi\n3.Kolkata\n4.Chennai\n");
+    scanf("%d",&answer);
+    if(answer==2){
+        score++;
+    }
+    printf("\n2.Which language is used for system programming?\n");
+    printf("1.C\n2.HTML\n3.CSS\n4.SQL\n");
+    scanf("%d",&answer);
+    if(answer==1){
+        score++;
+    }
+    printf("\n3 What is 5 + 7?\n");
+    printf("1.10\n2.11\n3.12\n4.13\n");
+    scanf("%d",&answer);
+    if(answer==3){
+        score++;
+    }
+    printf("\nYour Score:%d/3\n",score);
+    return 0;
+}
